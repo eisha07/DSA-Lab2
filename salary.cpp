65e8cc198fdd6568b1eed9c50963aebd1dfdd97c
eisha07 <149478652+eisha07@users.noreply.github.com>
2025-09-19 04:02:03 -0700

    Update salary.cpp

--- salary.cpp
+++ salary.cpp
@@ -2,29 +2,32 @@
 using namespace std;
 
 int main() {
-    int size;    
-    int *salArray = new int [size];
+	int size;
 
-    cout << "enter the size of the array";
-  cin >> size; 
+	cout << "enter number of employees: ";
+	cin >> size;
 
-    // Input salaries
-    for (int i = 0; i < size; i++) {
-        cout << "Enter Salary for employee " << (i+1) << ": ";
-        cin >> salArray[i];
-    }
+	int *salArray = new int[size];
+
+	
+	// Input salaries
+	for (int i = 0; i < size; i++) {
+		cout << "Enter Salary for employee " << (i + 1) << ": ";
+		cin >> salArray[i];
+	}
 
 
-    for (int i = 0; i < size; i++) {
-        salArray[i] = salArray[i] + salArray[i] / (i + 1);
-    }
+	for (int i = 0; i < size; i++) {
+		salArray[i] = salArray[i] + salArray[i] / (i + 1);
+	}
 
-    // Display updated salaries
-    cout << "\nUpdated Salaries: ";
-    for (int i = 0; i < size; i++) {
-        cout << salArray[i] << " ";
-    }
-    cout << endl;
+	// Display updated salaries
+	cout << "\nUpdated Salaries: ";
+	for (int i = 0; i < size; i++) {
+		cout << salArray[i] << " ";
+	}
+	cout << endl;
 
-    return 0;
+	system("pause");
+	return 0;
 }
